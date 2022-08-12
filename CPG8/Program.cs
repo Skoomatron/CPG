b19c1a7a490d6fd480b842bb5c35d232ff73db35
Trevor Edwards <trevor.jeffrey.edwards@gmail.com>
2022-08-12 18:57:29 -0400

    Modulo With If/Else Logic

---
+++ CPG8/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Please input a number.");
+int tickTock = 0;
+tickTock = Convert.ToInt32(Console.ReadLine());
+
+if (tickTock % 2 == 0)
+{
+    Console.WriteLine("Tick!");
+}
+else
+{
+    Console.WriteLine("Tock!");
+}
