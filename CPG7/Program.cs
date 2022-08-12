0b535c7d7e1cc43801def0af772f7a6c9ea0c4e5
Trevor Edwards <trevor.jeffrey.edwards@gmail.com>
2022-08-12 18:52:44 -0400

    Fun With Coordinates and Console Flare

---
+++ CPG7/Program.cs
@@ -0,0 +1,19 @@
+﻿Console.Title = "Defense of the Kingdom!";
+
+int targetRow = 0;
+int targetColumn = 0;
+
+Console.WriteLine("What is the target X cooridinate?");
+targetRow = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("What is the target Y cooridinate?");
+targetColumn = Convert.ToInt32(Console.ReadLine());
+
+Console.ForegroundColor = ConsoleColor.Magenta;
+
+Console.WriteLine("Deploy to ");
+Console.WriteLine("(" + (targetColumn - 1) + ", " + targetRow + ")");
+Console.WriteLine("(" + targetColumn + ", " + (targetRow + 1) + ")");
+Console.WriteLine("(" + targetColumn + ", " + (targetRow - 1) + ")");
+Console.WriteLine("(" + (targetColumn + 1) + ", " + targetRow + ")");
+
+Console.Beep();
