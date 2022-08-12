24502516999467aed94712d70767673789554bb2
Trevor Edwards <trevor.jeffrey.edwards@gmail.com>
2022-08-12 15:50:03 -0400

    Basic But Fun Exercise

---
+++ CPG2/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("What kind of thing are we talking about?");
+// User input of object
+string userObject = Console.ReadLine();
+Console.WriteLine("How would you describe it? Big? Azure? Tattered?");
+// User input of adverb
+string adverb = Console.ReadLine();
+// Descriptor
+string descriptor = "of Doom";
+// Model Number
+string modelNumber = "3000";
+
+Console.WriteLine("The " + adverb + " " + userObject + " of " + descriptor + " " + modelNumber + "!");
