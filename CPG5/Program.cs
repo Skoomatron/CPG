14d3245ffef876903adfce1845675bdaefec2eb7
Trevor Edwards <trevor.jeffrey.edwards@gmail.com>
2022-08-12 17:44:46 -0400

    Remainder Math

---
+++ CPG5/Program.cs
@@ -0,0 +1,7 @@
+﻿byte sisters = 4;
+Console.WriteLine("How many eggs did the sisters collect today?");
+byte eggsCollected = 0;
+eggsCollected = Byte.Parse(Console.ReadLine());
+float duckbearEggs = eggsCollected % sisters;
+float sisterEggs = (eggsCollected - duckbearEggs) / sisters;
+Console.WriteLine("The duckbear gets " + duckbearEggs + " eggs, the sisters each get " + sisterEggs + ".");
