0890ee42a83490e072c68fc51fd47f8d36408ed3
Trevor Edwards <trevor.jeffrey.edwards@gmail.com>
2022-08-12 15:34:09 -0400

    More Console Challenges - Might Have to Skip Ahead

--- CPG1/HelloWorld.cs
+++ CPG1/HelloWorld.cs
@@ -1,1 +1,5 @@
 ﻿Console.WriteLine("Haha, challenge accepted!");
+Console.WriteLine("These are silly challenges");
+Console.WriteLine("But this book is cool");
+Console.WriteLine("Guess I'll keep going");
+Console.WriteLine("Here is the 5th Line");
