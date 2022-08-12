272f3d61f41a874e6aea05fb3c1c9658520cb076
Trevor Edwards <trevor.jeffrey.edwards@gmail.com>
2022-08-12 17:21:52 -0400

    Testing MinValue/MaxValue

--- CPG4/Program.cs
+++ CPG4/Program.cs
@@ -6,3 +6,9 @@ float triangleHeight = 0.0f;
 triangleHeight = Single.Parse(Console.ReadLine());
 float result = (triangleBase * triangleHeight) / 2;
 Console.WriteLine("The Triangle's area is " + result + ".");
+
+short testingMin = short.MinValue;
+short testingMax = short.MaxValue;
+
+Console.WriteLine(testingMax);
+Console.WriteLine(testingMin);
