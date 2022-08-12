1f88b65ebf03bb1073cf0c7fa7e93147e45d52b4
Trevor Edwards <trevor.jeffrey.edwards@gmail.com>
2022-08-12 17:16:34 -0400

    Triangle Area Calculated

---
+++ CPG4/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.WriteLine("What is the triangle base length?");
+float triangleBase = 0.0f;
+triangleBase = Single.Parse(Console.ReadLine());
+Console.WriteLine("What is the triangle height?");
+float triangleHeight = 0.0f;
+triangleHeight = Single.Parse(Console.ReadLine());
+float result = (triangleBase * triangleHeight) / 2;
+Console.WriteLine("The Triangle's area is " + result + ".");
