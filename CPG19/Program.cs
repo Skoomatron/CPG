ddc3914f420b8ba20e51f7e088ec726daa3b113a
Trevor Edwards <trevor.jeffrey.edwards@gmail.com>
2022-08-13 11:04:37 -0400

    Tuples and Enum Exercise

---
+++ CPG19/Program.cs
@@ -0,0 +1,68 @@
+﻿(FoodType food, MainIngredient ingredient, Seasoning season) soup = GetSoup();
+Console.WriteLine($"{soup.season} {soup.ingredient} {soup.food}");
+
+(FoodType, MainIngredient, Seasoning) GetSoup()
+{
+    FoodType food = GetFoodType();
+    MainIngredient ingredient = GetMainIngredient();
+    Seasoning season = GetSeasoning();
+    return (food, ingredient, season);
+}
+
+FoodType GetFoodType()
+{
+    Console.Write("Soup Types (soup, stew, gumbo): ");
+    string input = Console.ReadLine();
+    return input switch
+    {
+        "soup" => FoodType.soup,
+        "stew" => FoodType.stew,
+        "gumbo" => FoodType.gumbo,
+    };
+}
+
+Seasoning GetSeasoning()
+{
+    Console.Write("Seasoning Types (spicy, salty, sweet): ");
+    string input = Console.ReadLine();
+    return input switch
+    {
+        "spicy" => Seasoning.spicy,
+        "salty" => Seasoning.salty,
+        "sweet" => Seasoning.sweet,
+    };
+}
+
+MainIngredient GetMainIngredient()
+{
+    Console.Write("Soup Types (mushroom, chicken, carrot, potato): ");
+    string input = Console.ReadLine();
+    return input switch
+    {
+        "mushroom" => MainIngredient.mushrooms,
+        "chicken" => MainIngredient.chicken,
+        "carrot" => MainIngredient.carrots,
+        "potato" => MainIngredient.potatoes
+    };
+}
+enum FoodType
+{
+    soup,
+    stew,
+    gumbo
+}
+
+enum MainIngredient
+{
+    mushrooms,
+    chicken,
+    carrots,
+    potatoes
+}
+
+enum Seasoning
+{
+    spicy,
+    salty,
+    sweet
+}
