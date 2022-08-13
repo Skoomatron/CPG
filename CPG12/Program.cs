09e77799f55d4be5673a532bdd25d3baa46dc69a
Trevor Edwards <trevor.jeffrey.edwards@gmail.com>
2022-08-13 07:46:09 -0400

    Looping With Modulo Checks

---
+++ CPG12/Program.cs
@@ -0,0 +1,20 @@
+﻿// divisible by 3 = fire
+// divisible by 5 = electric
+// divisible by both = combined blast
+
+for (int x = 0; x <= 100; x++)
+{
+    if (x % 3 == 0 && x % 5 == 0)
+    {
+        Console.ForegroundColor = ConsoleColor.Magenta;
+        Console.WriteLine("COMBINED BLAST at crank " + x);
+    } else if (x % 3 == 0 && x % 5 != 0)
+    {
+        Console.ForegroundColor = ConsoleColor.Red;
+        Console.WriteLine("FIRE BLAST at crank " + x);
+    } else if (x % 5 == 0 && x % 3 != 0)
+    {
+        Console.ForegroundColor = ConsoleColor.Cyan;
+        Console.WriteLine("ELECTRIC BLAST at crank " + x);
+    }
+}
