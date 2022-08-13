8107b1ffd7a168f556976d243b83101e65e26443
Trevor Edwards <trevor.jeffrey.edwards@gmail.com>
2022-08-13 08:33:55 -0400

    Methods for Range and Input

---
+++ CPG15/Program.cs
@@ -0,0 +1,22 @@
+﻿int AskForNumber(string text)
+{
+    Console.WriteLine(text);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+int AskForNumberInRange(int min, int max, string text)
+{
+    int input;
+    do
+    {
+        Console.WriteLine(text);
+        input = Convert.ToInt32(Console.ReadLine());
+    } while (input > max || input < min);
+
+    return input;
+}
+int result = AskForNumber("Please submit a number.");
+int askRange = AskForNumberInRange(0, 10, "Please guess a number in range.");
+
+Console.WriteLine(result);
+Console.WriteLine(askRange);
