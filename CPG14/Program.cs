a8086f41f64a90463221b5c6bc458f8556b9f9e8
Trevor Edwards <trevor.jeffrey.edwards@gmail.com>
2022-08-13 08:23:23 -0400

    Minimum and Average With ForEach Loop

---
+++ CPG14/Program.cs
@@ -0,0 +1,17 @@
+﻿int[] testArray = new int[5] {3, 2, 1, 4, 5};
+float average;
+int minimum = testArray[0];
+int total = 0;
+
+foreach (int index in testArray)
+{
+    total += index;
+    if (index < minimum)
+    {
+        minimum = index;
+    }
+}
+average = total / testArray.Length;
+
+Console.WriteLine("Your average is " + average);
+Console.WriteLine("Your minimum is " + minimum);
