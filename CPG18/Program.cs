262ef3b69f11ffa91bbbef040948ac6b4ec32698
Trevor Edwards <trevor.jeffrey.edwards@gmail.com>
2022-08-13 10:28:44 -0400

    Infinite Looping Enum Check

---
+++ CPG18/Program.cs
@@ -0,0 +1,65 @@
+﻿Chest magicChest = Chest.locked;
+bool forever = true;
+string answer;
+
+do
+{
+    if (magicChest == Chest.locked)
+    {
+        Console.WriteLine("The chest is locked, would you like to unlock it?");
+        answer = Console.ReadLine();
+        if (answer == "Yes")
+        {
+            magicChest = Chest.unlocked;
+        }
+        else
+        {
+            magicChest = Chest.locked;
+        }
+    } else if (magicChest == Chest.unlocked)
+    {
+        Console.WriteLine("The Magic Chest is unlocked, would you like to open it?");
+        answer = Console.ReadLine();
+        if (answer == "Yes")
+        {
+            magicChest = Chest.opened;
+        }
+        else
+        {
+            magicChest = Chest.unlocked;
+        }
+    } else if (magicChest == Chest.opened)
+    {
+        Console.WriteLine("The Magic Chest is opened, would you like to close it?");
+        answer = Console.ReadLine();
+        if (answer == "Yes")
+        {
+            magicChest = Chest.closed;
+        }
+        else
+        {
+            magicChest = Chest.opened;
+        }
+    } else if (magicChest == Chest.closed)
+    {
+        Console.WriteLine("The Magic Chest is closed, would you like to lock it?");
+        answer = Console.ReadLine();
+        if (answer == "Yes")
+        {
+            magicChest = Chest.locked;
+        }
+        else
+        {
+            magicChest = Chest.closed;
+        }
+    }
+} while (forever == true);
+
+
+enum Chest
+{
+    locked,
+    unlocked,
+    opened,
+    closed
+}
