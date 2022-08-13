7c0db8579b92b18f47c86cce65ec6830a53b8f30
Trevor Edwards <trevor.jeffrey.edwards@gmail.com>
2022-08-13 08:13:37 -0400

    Copying/Outputting Arrays

---
+++ CPG13/Program.cs
@@ -0,0 +1,19 @@
+﻿int[] originalArray = new int[5];
+int[] copiedArray = new int[5];
+
+int submittedNumber;
+
+for (int x = 0; x < originalArray.Length; x++)
+{
+    Console.WriteLine("Please input a number.");
+    submittedNumber = Convert.ToInt32(Console.ReadLine());
+    originalArray[x] = submittedNumber;
+}
+
+copiedArray = originalArray[0..5]; 
+
+for (int x = 0; x <= originalArray.Length - 1; x++)
+{
+    Console.WriteLine("The original Array contains " + originalArray[x]);
+    Console.WriteLine("The copied Array contains " + copiedArray[x]);
+}
