d860b6502bf980771cdfef5b71e3d6db56324ef8
Trevor Edwards <trevor.jeffrey.edwards@gmail.com>
2022-08-13 08:40:41 -0400

    Simple Recursion

---
+++ CPG16/Program.cs
@@ -0,0 +1,14 @@
+﻿void Recursion(int number)
+{
+    if (number == 0)
+    {
+        Console.WriteLine("You have reached 0");
+        return;
+    }
+
+    number = number - 1;
+    Recursion(number);
+}
+
+Recursion(10);
+
