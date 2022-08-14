a83332f1104b431665f1db5435e112309c7c23c5
Trevor Edwards <trevor.jeffrey.edwards@gmail.com>
2022-08-14 09:51:26 -0400

    Card Type Checker Working

--- CPG26/Program.cs
+++ CPG26/Program.cs
@@ -1,8 +1,35 @@
-﻿
+﻿Card newCard = new Card();
+Console.WriteLine("What type of card is the current card?");
+Console.WriteLine(newCard.CheckRank(newCard._cardRank));
+
+
 class Card
 {
     public CardColors _cardColor;
     public CardRanks _cardRank;
+
+    public Card()
+    {
+        _cardColor = CardColors.blue;
+        _cardRank = CardRanks.one;
+    }
+    public Card(CardColors color, CardRanks rank)
+    {
+        _cardColor = color;
+        _cardRank = rank;
+    }
+    public string CheckRank(CardRanks rank)
+    {
+        if (rank == CardRanks.ampersand || rank == CardRanks.dollar || rank == CardRanks.exponent ||
+            rank == CardRanks.percent)
+        {
+            return "Power Card!";
+        }
+        else
+        {
+            return "Number Card.";
+        }
+    }
     
 }
 enum CardColors
