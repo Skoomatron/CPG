18caff549b52ed0a82cf965cefa97499eb9deeb8
Trevor Edwards <trevor.jeffrey.edwards@gmail.com>
2022-08-14 09:41:18 -0400

    Beginning Card Deck With Classes and Enums

---
+++ CPG26/Program.cs
@@ -0,0 +1,32 @@
+﻿
+class Card
+{
+    public CardColors _cardColor;
+    public CardRanks _cardRank;
+    
+}
+enum CardColors
+{
+    red,
+    blue,
+    green,
+    yellow
+}
+
+enum CardRanks
+{
+    one,
+    two,
+    three,
+    four,
+    five,
+    six,
+    seven,
+    eight,
+    nine,
+    ten,
+    dollar,
+    percent,
+    exponent,
+    ampersand
+}
